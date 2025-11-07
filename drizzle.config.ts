ce190203f4fd0a38601de061c35f02b26d16645a
Brian Babiak MD <bdbabiak@gmail.com>
2025-11-06 22:15:09 -0500

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,11 +5,24 @@ if (!connectionString) {
   throw new Error("DATABASE_URL is required to run drizzle commands");
 }
 
+// Parse the connection string to extract components
+const url = new URL(connectionString.replace('mysql://', 'http://'));
+const [username, password] = url.username && url.password 
+  ? [url.username, url.password]
+  : url.username.split(':');
+
 export default defineConfig({
   schema: "./drizzle/schema.ts",
   out: "./drizzle",
   dialect: "mysql",
   dbCredentials: {
-    url: connectionString,
+    host: url.hostname,
+    port: parseInt(url.port) || 3306,
+    user: username,
+    password: password,
+    database: url.pathname.slice(1),
+    ssl: {
+      rejectUnauthorized: true
+    }
   },
 });
