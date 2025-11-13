3e7ffce395133521ab40d82c4909c72bcdab55a6
Brian Babiak MD <bdbabiak@gmail.com>
2025-11-12 19:59:42 -0500

    Update StickyBuyBar.tsx

--- client/src/components/StickyBuyBar.tsx
+++ client/src/components/StickyBuyBar.tsx
@@ -38,13 +38,7 @@ export const StickyBuyBar: React.FC<StickyBuyBarProps> = ({
 
   const handleBuyClick = () => {
     if (currentBook) {
-      // MODIFIED: Report conversion on click
-      if (typeof (window as any).gtag === 'function') {
-        (window as any).gtag('event', 'conversion', {
-            'send_to': 'AW-17609588022/AtoCCI22g70bELb688xB',
-        });
-      }
-
+      // The gtag call is now handled by Analytics.amazonClick
       Analytics.amazonClick(currentBook.asin, currentBook.title);
       window.open(currentBook.amazonLink, '_blank');
     }
